8635ce3e4de735873a4d13bc74cd43033a59846a
Natasha Murashkina <murnatty@gmail.com>
2017-10-11 00:38:32 +0300

    hw05 Set: initial

---
+++ hw05_set/src/test/java/SetTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+public class SetTest {
+    @org.junit.Test
+    public void addOne() throws Exception {
+        Set<Integer> set = new Set<Integer>();
+        set.add(5);
+        assertEquals(1, set.size());
+        assertTrue(set.contains(5));
+        assertFalse(set.contains(0));
+    }
+
+    @org.junit.Test
+    public void addSameTwice() throws Exception {
+        Set<Integer> set = new Set<Integer>();
+        set.add(5);
+        set.add(5);
+        assertEquals(1, set.size());
+        assertTrue(set.contains(5));
+        assertFalse(set.contains(0));
+    }
+
+    @org.junit.Test
+    public void addMany() throws Exception {
+        Set<Integer> set = new Set<Integer>();
+        Integer[] data = {1, 2};
+        for (Integer x : data)
+            set.add(x);
+        assertEquals(data.length, set.size());
+        for (Integer x : data)
+            assertTrue(set.contains(x));
+        assertFalse(set.contains(0));
+    }
+
+    @org.junit.Test
+    public void containsNegativeEmpty() throws Exception {
+        Set<Integer> set = new Set<Integer>();
+        assertFalse(set.contains(0));
+        assertFalse(set.contains(1));
+    }
+
+    @org.junit.Test
+    public void sizeEmpty() throws Exception {
+        Set<Integer> set = new Set<Integer>();
+        assertEquals(0, set.size());
+    }
+
+}
