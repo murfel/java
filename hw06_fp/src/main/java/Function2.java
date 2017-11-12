035b7e5ab575a3eb57deb90e542ac9ad441b47b0
murfel <murnatty@gmail.com>
2017-11-12 04:45:46 +0300

    hw06_fp: add docs

--- hw06_fp/src/main/java/Function2.java
+++ hw06_fp/src/main/java/Function2.java
@@ -1,6 +1,29 @@
+/**
+ * Represents a function that accepts two arguments and produces a result.
+ * This is the two-arity specialization of Function1.
+ *
+ * @param <A>  the type of the first argument to the function
+ * @param <B>  the type of the second argument to the function
+ * @param <C>  the type of the result of the function
+ */
 public abstract class Function2<A, B, C> {
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param a  the first function argument
+     * @param b  the second function argument
+     * @return the function result
+     */
     public abstract C apply(A a, B b);
 
+    /**
+     * Returns a composed function that first applies this function to its input,
+     * and then applies the after function to the result.
+     *
+     * @param g  the function to apply after this function is applied
+     * @param <D>  the type of output of the after function, and of the composed function
+     * @return a composed function that first applies this function and then applies the after function
+     */
     public <D> Function2<A, B, D> compose(Function1<C, D> g) {
         return new Function2<A, B, D>() {
             @Override
@@ -10,6 +33,12 @@ public abstract class Function2<A, B, C> {
         };
     }
 
+    /**
+     * Apply this function partially binding its first argument to always equal to a.
+     *
+     * @param a  the value to bind the first argument to
+     * @return a one argument function based on this function with its first argument bound to a
+     */
     public Function1<B, C> bind1(A a) {
         return new Function1<B, C>() {
             @Override
@@ -19,6 +48,12 @@ public abstract class Function2<A, B, C> {
         };
     }
 
+    /**
+     * Apply this function partially binding its second argument to always equal to b.
+     *
+     * @param b  the value to bind the second argument to
+     * @return b one argument function based on this function with its second argument bound to b
+     */
     public Function1<A, C> bind2(B b) {
         return new Function1<A, C>() {
             @Override
@@ -28,6 +63,12 @@ public abstract class Function2<A, B, C> {
         };
     }
 
+    /**
+     * Curry the function transforming it into a one-argument function.
+     * For example, currying f = (x, y) -> x + y gives f = (x) -> y -> x + y.
+     *
+     * @return a one-argument function which returns a one-argument function
+     */
     public Function1<A, Function1<B, C>> curry() {
         return new Function1<A, Function1<B, C>>() {
             @Override
--- hw06_fp/src/main/java/Predicate.java
+++ hw06_fp/src/main/java/Predicate.java
@@ -1,5 +1,18 @@
+/**
+ * Represents a predicate (boolean-valued function) of one argument.
+ *
+ * @param <T>  the type of the input to the predicate
+ */
 public abstract class Predicate<T> extends Function1<T, Boolean> {
 
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical OR of this predicate and another.
+     * When evaluating the composed predicate, if this predicate is true, then the other predicate is not evaluated.
+     *
+     * @param p  a predicate that will be logically-ORed with this predicate
+     * @return a composed predicate that represents the short-circuiting logical OR
+     * of this predicate and the other predicate
+     */
     public Predicate<T> or(Predicate<T> p) {
         return new Predicate<T>() {
             @Override
@@ -9,6 +22,14 @@ public abstract class Predicate<T> extends Function1<T, Boolean> {
         };
     }
 
+    /**
+     * Returns a composed predicate that represents a short-circuiting logical AND of this predicate and another.
+     * When evaluating the composed predicate, if this predicate is false, then the other predicate is not evaluated.
+     *
+     * @param p  a predicate that will be logically-ANDed with this predicate
+     * @return a composed predicate that represents the short-circuiting logical AND
+     * of this predicate and the other predicate
+     */
     public Predicate<T> and(Predicate<T> p) {
         return new Predicate<T>() {
             @Override
@@ -18,6 +39,11 @@ public abstract class Predicate<T> extends Function1<T, Boolean> {
         };
     }
 
+    /**
+     * Returns a predicate that represents the logical negation of this predicate.
+     *
+     * @return a predicate that represents the logical negation of this predicate
+     */
     public Predicate<T> not() {
         return new Predicate<T>() {
             @Override
@@ -27,15 +53,25 @@ public abstract class Predicate<T> extends Function1<T, Boolean> {
         };
     }
 
-    public static <X> Predicate<X> ALWAYS_TRUE() {
-        return new Predicate<X>() {
+    /**
+     * A predicate which always returns Boolean.TRUE.
+     *
+     * @return a predicate which always returns Boolean.TRUE
+     */
+    public static Predicate ALWAYS_TRUE() {
+        return new Predicate() {
             @Override
-            public Boolean apply(X t) {
+            public Boolean apply(Object t) {
                 return true;
             }
         };
     }
 
+    /**
+     * A predicate which always returns Boolean.FALSE.
+     *
+     * @return a predicate which always returns Boolean.FALSE
+     */
     public static Predicate ALWAYS_FALSE() {
         return ALWAYS_TRUE().not();
     }
