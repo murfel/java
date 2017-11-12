6c54b532ac363d043a6c97dba6a84459254c7ade
murfel <murnatty@gmail.com>
2017-11-12 04:18:25 +0300

    hw06_fp: add tests, fix .iml

---
+++ hw06_fp/src/test/java/Function1Test.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+public class Function1Test {
+    @org.junit.Test
+    public void apply() throws Exception {
+        Function1<Integer, Boolean> f = new Function1<Integer, Boolean>() {
+            public Boolean apply(Integer integer) {
+                return integer % 2 == 0;
+            }
+        };
+        assertEquals(Boolean.TRUE, f.apply(0));
+        assertEquals(Boolean.FALSE, f.apply(1));
+    }
+
+    @org.junit.Test
+    public void compose() throws Exception {
+        Function1<Integer, Boolean> f = new Function1<Integer, Boolean>() {
+            public Boolean apply(Integer integer) {
+                return integer % 2 == 0;
+            }
+        };
+
+        Function1<Boolean, String> g = new Function1<Boolean, String>() {
+            @Override
+            public String apply(Boolean aBoolean) {
+                return aBoolean.toString();
+            }
+        };
+
+        assertEquals("true", f.compose(g).apply(0));
+        assertEquals("false", f.compose(g).apply(1));
+    }
+
+}
---
+++ hw06_fp/src/test/java/Function2Test.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Function2Test {
+    private Function1<String, Character> g = new Function1<String, Character>() {
+        @Override
+        public Character apply(String s) {
+            return !s.isEmpty() ? s.charAt(0) : null;
+        }
+    };
+
+    private Function2<Integer, Boolean, String> f = new Function2<Integer, Boolean, String>() {
+        @Override
+        public String apply(Integer integer, Boolean aBoolean) {
+            return integer.toString() + aBoolean.toString();
+        }
+    };
+
+    @Test
+    public void apply() throws Exception {
+        assertEquals("2true", f.apply(2, Boolean.TRUE));
+    }
+
+    @Test
+    public void compose() throws Exception {
+        assertEquals(new Character('2'), f.compose(g).apply(2, Boolean.TRUE));
+    }
+
+    @Test
+    public void bind1() throws Exception {
+        assertEquals("2true", f.bind1(2).apply(Boolean.TRUE));
+    }
+
+    @Test
+    public void bind2() throws Exception {
+        assertEquals("4false", f.bind2(Boolean.FALSE).apply(4));
+    }
+
+    @Test
+    public void curry() throws Exception {
+        assertEquals("4false", f.curry().apply(4).apply(Boolean.FALSE));
+    }
+
+}
