45fd08f553503fd7be8476122776ef8ef2c1a667
Natasha Murashkina <murnatty@gmail.com>
2017-12-17 03:31:47 +0300

    add template and smoke tests

---
+++ letuchka3/src/main/java/Point.java
@@ -0,0 +1,11 @@
+public class Point {
+    Point(double x, double y, double mass) {
+        this.x = x;
+        this.y = y;
+        this.mass = mass;
+    }
+
+    double x;
+    double y;
+    double mass;
+}
