fa5b0d2b46cade959394f6d0d34a32e4782329b7
Natasha Murashkina <murnatty@gmail.com>
2017-12-22 22:10:08 +0300

    implement by extending AbstractList, pass provided tests

---
+++ Test3SmartListPlus/src/main/java/SmartList.java
@@ -0,0 +1,188 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class SmartList<E> extends AbstractList<E> {
+    /**
+     * Constructs an empty list.
+     */
+    public SmartList() {
+    }
+
+    /**
+     * Constructs a list containing the elements of the specified collection,
+     * in the order they are returned by the collection's iterator.
+     *
+     * @param collection  the collection whose elements are to be placed into this list
+     * @throws NullPointerException  if the specified collection is null
+     */
+    public SmartList(Collection<E> collection) {
+        size = collection.size();
+        if (size == 0) {
+            return;
+        } else if (size == 1) {
+            data = collection.iterator().next();
+        } else if (size <= 5) {
+            data = collection.toArray();
+        } else {
+            data = new ArrayList<>(collection);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param index
+     * @throws IndexOutOfBoundsException {@inheritDoc}
+     */
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == 1) {
+            @SuppressWarnings("unchecked")
+            E element = (E) data;
+            return element;
+        }
+        if (size <= 5) {
+            @SuppressWarnings("unchecked")
+            E[] elements = (E[]) data;
+            return elements[index];
+        }
+        @SuppressWarnings("unchecked")
+        ArrayList<E> arrayList = (ArrayList<E>) data;
+        return arrayList.get(index);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     *
+     * @param index
+     * @param element
+     * @throws ClassCastException            {@inheritDoc}
+     * @throws NullPointerException          {@inheritDoc}
+     * @throws IllegalArgumentException      {@inheritDoc}
+     * @throws IndexOutOfBoundsException     {@inheritDoc}
+     */
+    @Override
+    public E set(int index, E element) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == 1) {
+            @SuppressWarnings("unchecked")
+            E oldElement = (E) data;
+            data = element;
+            return oldElement;
+        }
+        if (size <= 5) {
+            @SuppressWarnings("unchecked")
+            E[] array = (E[]) data;
+            E oldElement = array[index];
+            array[index] = element;
+            data = array;
+            return oldElement;
+        }
+        @SuppressWarnings("unchecked")
+        ArrayList<E> arrayList = (ArrayList<E>) data;
+        return arrayList.set(index, element);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     *
+     * @param index
+     * @param element
+     * @throws ClassCastException            {@inheritDoc}
+     * @throws NullPointerException          {@inheritDoc}
+     * @throws IllegalArgumentException      {@inheritDoc}
+     * @throws IndexOutOfBoundsException     {@inheritDoc}
+     */
+    @Override
+    public void add(int index, E element) {
+        if (index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == 0) {
+            data = element;
+        } else if (size == 1) {
+            Object[] newData = new Object[5];
+            newData[index] = element;
+            newData[1 - index] = data;
+            data = newData;
+        } else if (size < 5) {
+            @SuppressWarnings("unchecked")
+            E[] array = (E[]) data;
+            for (int i = size - 1; i >= index; size++) {
+                array[i + 1] = array[i];
+            }
+            System.arraycopy(array, index, array, index + 1, size - index);
+            array[index] = element;
+            data = array;
+        } else if (size == 5) {
+            @SuppressWarnings("unchecked")
+            E[] array = (E[]) data;
+            ArrayList<E> arrayList = new ArrayList<>(Arrays.asList(array));
+            arrayList.add(index, element);
+            data = arrayList;
+        } else {
+            @SuppressWarnings("unchecked")
+            ArrayList<E> arrayList = (ArrayList<E>) data;
+            arrayList.add(index, element);
+        }
+        size++;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     *
+     * @param index
+     * @throws IndexOutOfBoundsException     {@inheritDoc}
+     */
+    @Override
+    public E remove(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == 1) {
+            size--;
+            @SuppressWarnings("unchecked")
+            E element = (E) data;
+            data = null;
+            return element;
+        }
+        E element;
+        if (size <= 5) {
+            @SuppressWarnings("unchecked")
+            E[] array = (E[]) data;
+            element = array[index];
+            if (size == 2) {
+                data = array[1 - index];
+            } else {
+                System.arraycopy(array, index + 1, array, index, size - index - 1);
+            }
+        } else {
+            @SuppressWarnings("unchecked")
+            ArrayList<E> arrayList = (ArrayList<E>) data;
+            element = arrayList.remove(index);
+            if (size == 6) {
+                data = arrayList.toArray();
+            }
+        }
+        size--;
+        return element;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private int size;
+    private Object data;
+}
